f77e079edef7185a875e7f0987b63c4309b2c656
DzinToDev <maheshdgaikwad18@gmail.com>
2025-05-11 14:36:16 +0530

    pointer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ children }) => {
   return (
-    <div className="text-[10px] px-5 py-2 w-fit bg-white text-black mt-4 rounded-sm font-FragmentMono">
+    <div className="text-[10px] px-5 py-2 w-fit bg-white text-black mt-4 rounded-sm font-FragmentMono cursor-pointer">
       {children}
     </div>
   );
